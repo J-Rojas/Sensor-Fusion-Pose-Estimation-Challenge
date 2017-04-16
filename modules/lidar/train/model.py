a774375521d08fab209a2664161c304cf2cb4435
joanxiao <joanxiao@users.noreply.github.com>
2017-04-15 19:42:46 -0700

    corrected input and output dimensions, and changed down/up sampling ratio to 2:4.
    down/up sampling ratio should be 2:4 according to the paper for HDL-64E. Previous ratio was 2:16 (assuming HDL-32E is used), however the projected 2D lidar images do not work well after changing the code to use HDL-32E configurations. Staying with 64E for now.

--- modules/lidar/train/model.py
+++ modules/lidar/train/model.py
@@ -13,23 +13,25 @@ from keras.layers.convolutional import Conv2D, MaxPooling2D, Conv2DTranspose
 from keras.models import Model
 
 
-INPUT_SHAPE = (93, 1029, 4)
+INPUT_SHAPE = (93, 1029, 2)
 
 def build_model():
     inputs = Input(shape=INPUT_SHAPE, name="input")
-    conv1 = Conv2D(24, 5, strides=(2,16), activation='relu', name='conv1')(inputs)
-    conv2 = Conv2D(36, 5, strides=(2,2), activation='relu', name='conv2')(conv1)
-    conv3 = Conv2D(48, 5, strides=(2,2), activation='relu', name='conv3')(conv2)
-    deconv4 = Conv2DTranspose(36, 5, strides=(2,2), activation='relu', name='deconv4')(conv3)
+    conv1 = Conv2D(4, 5, strides=(2,4), activation='relu', name='conv1')(inputs)
+    conv2 = Conv2D(6, 5, strides=(2,2), activation='relu', name='conv2')(conv1)
+    conv3 = Conv2D(12, 5, strides=(2,2), activation='relu', name='conv3')(conv2)
+    deconv4 = Conv2DTranspose(16, 5, strides=(2,2), activation='relu', name='deconv4')(conv3)
     concat_deconv4 = concatenate([conv2, deconv4], name='concat_deconv4')
 
-    deconv5a = Conv2DTranspose(24, 5, strides=(2,2), activation='relu', name='deconv5a')(concat_deconv4)
+    #classification task
+    deconv5a = Conv2DTranspose(8, 5, strides=(2,2), activation='relu', name='deconv5a')(concat_deconv4)
     concat_deconv5a = concatenate([conv1, deconv5a], name='concat_deconv5a')
-    deconv6a = Conv2DTranspose(24, 5, strides=(2,16), activation='relu', name='deconv6a')(concat_deconv5a)
+    deconv6a = Conv2DTranspose(2, 5, strides=(2,4), activation='relu', name='deconv6a')(concat_deconv5a)
 
+    #regression task
     deconv5b = Conv2DTranspose(24, 5, strides=(2,2), activation='relu', name='deconv5b')(concat_deconv4)
     concat_deconv5b = concatenate([conv1, deconv5b], name='concat_deconv5b')
-    deconv6b = Conv2DTranspose(24, 5, strides=(2,16), activation='relu', name='deconv6b')(concat_deconv5b)
+    deconv6b = Conv2DTranspose(24, 5, strides=(2,4), activation='relu', name='deconv6b')(concat_deconv5b)
 
     model = Model(inputs=inputs, outputs=[deconv6a, deconv6b])
     print(model.summary())
